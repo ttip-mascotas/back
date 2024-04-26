cabd7243d4972de6c373c96c277979a3ea5b55ab
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-26 04:59:23 -0300

    Habilita las propiedades de Minio

--- src/main/kotlin/org/pets/history/HistoryApplication.kt
+++ src/main/kotlin/org/pets/history/HistoryApplication.kt
@@ -1,9 +1,12 @@
 package org.pets.history
 
+import org.pets.history.configuration.properties.MinioProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(MinioProperties::class)
 class HistoryApplication
 
 fun main(args: Array<String>) {

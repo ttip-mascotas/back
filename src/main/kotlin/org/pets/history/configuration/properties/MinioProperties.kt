44397dea394dd182ca3e007697e93fe115e6a1cd
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-26 04:58:42 -0300

    Agrega propiedades de Minio

---
+++ src/main/kotlin/org/pets/history/configuration/properties/MinioProperties.kt
@@ -0,0 +1,34 @@
+package org.pets.history.configuration.properties
+
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.URL
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
+import org.springframework.validation.annotation.Validated
+
+@Validated
+@ConfigurationProperties(prefix = "minio")
+@ConfigurationPropertiesBinding
+data class MinioProperties(
+    @URL(protocol = "http")
+    @Value("\${host}")
+    val host: String,
+
+    @NotBlank
+    @Value("\${username}")
+    val username: String,
+
+    @NotBlank
+    @Value("\${password}")
+    val password: String,
+
+    @NotBlank
+    @Value("\${public_bucket}")
+    val publicBucket: String,
+
+    @Max(value = 15728640)
+    @Value("\${put_object_part_size}")
+    val putObjectPartSize: Long,
+)

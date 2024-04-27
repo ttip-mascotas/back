6a0adaaa6c92c93ab3675c337d6f863bd39a4f68
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-27 16:51:21 -0300

    Agrega pruebas unitarias

--- src/test/kotlin/org/pets/history/service/MinioServiceTest.kt
+++ src/test/kotlin/org/pets/history/service/MinioServiceTest.kt
@@ -9,6 +9,9 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -24,10 +27,10 @@ class MinioServiceTest {
     @Autowired
     private lateinit var service: MinioService
 
-    @Test
-    fun `Uploads a JPG avatar successfully`() {
+    @ParameterizedTest
+    @MethodSource("avatarProvider")
+    fun `Uploads an avatar successfully`(bucket: String, fileExtension: String, mediaType: String) {
         val filename = UUID.randomUUID().toString()
-        val bucket = "public"
         val putObjectResult = mockk<ObjectWriteResponse>()
 
         every { putObjectResult.bucket() } returns bucket
@@ -37,8 +40,8 @@ class MinioServiceTest {
 
         val file = MockMultipartFile(
             "avatar",
-            "avatar.jpg",
-            MediaType.IMAGE_JPEG_VALUE,
+            "avatar.$fileExtension",
+            mediaType,
             "an_image".toByteArray()
         )
 
@@ -50,5 +53,44 @@ class MinioServiceTest {
         verify { minioClient.bucketExists(any()) }
 
         confirmVerified(minioClient)
+    }
+
+    @Test
+    fun `Uploads a pet analysis successfully`() {
+        val filename = UUID.randomUUID().toString()
+        val putObjectResult = mockk<ObjectWriteResponse>()
+        val petId = 1L
+        val bucket = "analysis"
+        val mediaType = MediaType.APPLICATION_PDF_VALUE
+        val fileExtension = "pdf"
+
+        every { putObjectResult.bucket() } returns bucket
+        every { putObjectResult.`object`() } returns "$petId/$filename"
+        every { minioClient.putObject(any()) } returns putObjectResult
+        every { minioClient.bucketExists(any()) } returns true
+
+        val file = MockMultipartFile(
+            "avatar",
+            "avatar.$fileExtension",
+            mediaType,
+            "a_pdf_file".toByteArray()
+        )
+
+        val analysisURL = service.uploadPetAnalysis(petId, file.inputStream, file.contentType!!)
+
+        assertEquals("http://127.0.0.1:9000/$bucket/$petId/$filename", analysisURL)
+
+        verify { minioClient.putObject(any()) }
+        verify { minioClient.bucketExists(any()) }
+
+        confirmVerified(minioClient)
+    }
+
+    companion object {
+        @JvmStatic
+        fun avatarProvider(): List<Arguments> = listOf(
+            Arguments.of("public", "jpg", MediaType.IMAGE_JPEG_VALUE),
+            Arguments.of("public", "png", MediaType.IMAGE_PNG_VALUE),
+        )
     }
 }

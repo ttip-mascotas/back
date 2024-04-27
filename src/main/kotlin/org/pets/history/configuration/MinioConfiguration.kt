6a15f8cee9004c6da424a592b32bcafceeb35012
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-26 23:56:55 -0300

    Elimina chequeo de existencia de bucket

--- src/main/kotlin/org/pets/history/configuration/MinioConfiguration.kt
+++ src/main/kotlin/org/pets/history/configuration/MinioConfiguration.kt
@@ -1,7 +1,5 @@
 package org.pets.history.configuration
 
-import io.minio.BucketExistsArgs
-import io.minio.MakeBucketArgs
 import io.minio.MinioClient
 import org.pets.history.configuration.properties.MinioProperties
 import org.springframework.context.annotation.Bean
@@ -16,9 +14,6 @@ class MinioConfiguration(private val minioProperties: MinioProperties) {
             .endpoint(minioProperties.host)
             .credentials(minioProperties.username, minioProperties.password)
             .build()
-        if (!client.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.publicBucket).build())) {
-            client.makeBucket(MakeBucketArgs.builder().bucket(minioProperties.publicBucket).build())
-        }
         return client
     }
 }

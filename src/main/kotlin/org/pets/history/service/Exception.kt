6925bb58072b0b6186cec2d3fdd59500b84d163f
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-27 15:10:00 -0300

    Agrega error de tipo de archivo

--- src/main/kotlin/org/pets/history/service/Exception.kt
+++ src/main/kotlin/org/pets/history/service/Exception.kt
@@ -1,3 +1,12 @@
 package org.pets.history.service
 
 class NotFoundException(override val message: String) : RuntimeException(message)
+
+class MediaTypeNotValidException(invalidMediaType: String, validMediaTypes: Iterable<String>) :
+    RuntimeException(
+        "El tipo de archivo $invalidMediaType no se encuentra dentro de los tipos de archivo soportados: ${
+            validMediaTypes.joinToString(
+                ", "
+            )
+        }"
+    )

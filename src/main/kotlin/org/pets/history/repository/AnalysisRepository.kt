31ae72a51e50ff65b448b96593485a3ccdd7bba2
Pablo <2845728+pablospizzamiglio@users.noreply.github.com>
2024-05-11 04:44:34 -0300

    Obtener análisis filtrados (#15)
    * Agrega query nativa para full text search
    
    * Elimina dependencia innecesaria
    
    * Agrega consulta nativa para poder utilizar la sintáxis de Postgres para full-text search
    
    * Expone método de búsqueda en servicio
    
    * Exponse endpoint de búsqueda de análisis
    
    * Evita exponer repositorios como endpoints y corrige configuración de OpenAPI
    
    * Capitaliza funciones SQL
    
    * Agrega TODO para consultar en checkpoint

--- src/main/kotlin/org/pets/history/repository/AnalysisRepository.kt
+++ src/main/kotlin/org/pets/history/repository/AnalysisRepository.kt
@@ -1,6 +1,19 @@
 package org.pets.history.repository
 
 import org.pets.history.domain.Analysis
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
-interface AnalysisRepository : CrudRepository<Analysis, Long>
+interface AnalysisRepository : CrudRepository<Analysis, Long> {
+    @Query(
+        value = """
+            SELECT * 
+            FROM Analysis a 
+            WHERE a.pet_id = :petId 
+            AND TO_TSVECTOR('spanish', a.name || ' ' || a.text) @@ WEBSEARCH_TO_TSQUERY('spanish', :query)
+        """,
+        nativeQuery = true
+    )
+    fun search(@Param("petId") petId: Long, @Param("query") query: String): Iterable<Analysis>
+}

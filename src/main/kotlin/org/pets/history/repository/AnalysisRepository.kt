1ef5734366c11df51158dbf6b7084be03fefe084
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-05-11 00:47:56 -0300

    Agrega query nativa para full text search

--- src/main/kotlin/org/pets/history/repository/AnalysisRepository.kt
+++ src/main/kotlin/org/pets/history/repository/AnalysisRepository.kt
@@ -1,6 +1,18 @@
 package org.pets.history.repository
 
 import org.pets.history.domain.Analysis
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
-interface AnalysisRepository : CrudRepository<Analysis, Long>
+interface AnalysisRepository : CrudRepository<Analysis, Long> {
+    @Query(
+        value = """
+            SELECT * 
+            FROM Analysis a 
+            WHERE to_tsvector('spanish', a.name || ' ' || a.text) @@ websearch_to_tsquery('spanish', :query)
+        """,
+        nativeQuery = true
+    )
+    fun search(@Param("query") query: String): List<Analysis>
+}

15e4b6f39f4b8155dd80752b9686833dd78cfc2a
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-05-18 00:35:30 -0300

    Ordena los resultados de las listas de entidades por fecha

--- src/main/kotlin/org/pets/history/domain/SchedulePerDay.kt
+++ src/main/kotlin/org/pets/history/domain/SchedulePerDay.kt
@@ -1,4 +1,5 @@
 package org.pets.history.domain
+
 import jakarta.persistence.*
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
@@ -8,10 +9,13 @@ class SchedulePerDay {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
+
     @Column(nullable = false)
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     lateinit var date: LocalDate
+
+    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
+    @OrderBy(value = "time ASC")
     @JoinColumn(name = "schedule_per_day_id")
-    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.EAGER)
-    lateinit var doseControllers: MutableSet<DoseControl>
+    var doseControls: MutableSet<DoseControl> = mutableSetOf()
 }

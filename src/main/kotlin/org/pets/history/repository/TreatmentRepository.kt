2d584b3202de77f95a66056e98a8a38cab1d13fe
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-05-18 00:36:18 -0300

    Utiliza un grafo de entidades para optimizar consultas

--- src/main/kotlin/org/pets/history/repository/TreatmentRepository.kt
+++ src/main/kotlin/org/pets/history/repository/TreatmentRepository.kt
@@ -1,6 +1,14 @@
 package org.pets.history.repository
 
 import org.pets.history.domain.Treatment
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
-interface TreatmentRepository : CrudRepository<Treatment, Long>
+interface TreatmentRepository : CrudRepository<Treatment, Long> {
+    @EntityGraph(
+        attributePaths = ["schedulesPerDay", "schedulesPerDay.doseControls"],
+        type = EntityGraph.EntityGraphType.FETCH,
+    )
+    fun findWithAllRelatedById(petId: Long): Optional<Treatment>
+}
--- src/main/kotlin/org/pets/history/service/TreatmentService.kt
+++ src/main/kotlin/org/pets/history/service/TreatmentService.kt
@@ -1,4 +1,5 @@
 package org.pets.history.service
+
 import jakarta.transaction.Transactional
 import org.pets.history.domain.Treatment
 import org.pets.history.repository.TreatmentRepository
@@ -6,12 +7,9 @@ import org.springframework.stereotype.Service
 
 @Service
 @Transactional(Transactional.TxType.SUPPORTS)
-class TreatmentService(
-        private val treatmentRepository: TreatmentRepository
-) {
-
+class TreatmentService(private val treatmentRepository: TreatmentRepository) {
     fun getTreatment(treatmentId: Long): Treatment {
-        return treatmentRepository.findById(treatmentId).orElseThrow {
+        return treatmentRepository.findWithAllRelatedById(treatmentId).orElseThrow {
             NotFoundException("No existe el tratamiento con identificador $treatmentId")
         }
     }

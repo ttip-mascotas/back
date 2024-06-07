38ede78684a0003714b5b853964cce5cd858016e
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-06-07 05:11:15 -0300

    Configure websockets

---
+++ src/main/kotlin/org/pets/history/configuration/WebSocketConfiguration.kt
@@ -0,0 +1,20 @@
+package org.pets.history.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfiguration : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.setApplicationDestinationPrefixes("/app")
+        config.enableSimpleBroker("/topic")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/notifications").setAllowedOrigins("*")
+    }
+}

1b1585a46a982b01400238709f40831d69eb2ed5
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-06-22 02:17:01 -0300

    Fix tests

--- src/test/kotlin/org/pets/history/controller/IntegrationTest.kt
+++ src/test/kotlin/org/pets/history/controller/IntegrationTest.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -15,6 +16,7 @@ import java.time.format.DateTimeFormatter
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @Testcontainers
+@WithMockUser
 abstract class IntegrationTest {
     @Autowired
     protected lateinit var mockMvc: MockMvc
